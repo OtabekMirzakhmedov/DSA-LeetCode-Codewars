c9d93382d2bb8b9ee213fc71a57817e506a04394
ITRANSITION\o.mirzakhmedov <o.mirzakhmedov@itransition.com>
2025-02-19 11:07:39 +0500

    second version Singleton Pattern

--- SingletonPattern/SingletonPattern.cs
+++ SingletonPattern/SingletonPattern.cs
@@ -1,90 +1,77 @@
-
 using System;
+using System.Linq;
+
+public sealed class Eve : Female
+{
+    private static Eve eve;
+
+    private Eve(): base("Eve")
+    {
+    }
+
+    public static Eve GetInstance(Adam adam)
+    {
+        if (adam==null)
+        {
+            throw new ArgumentNullException();
+        }
+        return eve??(eve=new Eve());
+    }
+}
+
 public abstract class Human
 {
-    public string Name { get; protected set; }
-    public Female Mother { get; protected set; }
-    public Male Father { get; protected set; }
-
-    protected Human(string name, Female mother, Male father)
+    protected Human(string name)
     {
-        ValidateParameters(name, mother, father);
         Name = name;
-        Mother = mother;
-        Father = father;
     }
 
-    private void ValidateParameters(string name, Female mother, Male father)
+    public Human(string name, Female mother, Male father):this(name)
     {
-        if (name == null)
-            throw new ArgumentNullException(nameof(name));
-
-        if (name == null)
-            throw new ArgumentNullException(nameof(name));
-    
-        // Allow null parents only for the true singleton instances of Adam and Eve.
-        if ((name == "Adam" && this.GetType() == typeof(Adam)) ||
-            (name == "Eve" && this.GetType() == typeof(Eve)))
-        {
-            return;
-        }
-        if (mother == null)
-            throw new ArgumentNullException(nameof(mother));
-        
-        if (father == null)
-            throw new ArgumentNullException(nameof(father));
+        if (mother==null) throw new ArgumentNullException("mother");
+        if (father==null) throw new ArgumentNullException("father");
+        Mother = mother;
+        Father = father; 
     }
+
+    public string Name { get; }
+    public Male Father { get; }
+    public Female Mother { get; }
 }
 
 public class Male : Human
 {
-    public Male(string name, Female mother, Male father) : base(name, mother, father)
+    protected Male(string name):base(name)
+    {
+    }
+
+    public Male(string name, Female mother, Male father):base(name, mother, father)
     {
     }
 }
 
 public class Female : Human
 {
-    public Female(string name, Female mother, Male father) : base(name, mother, father)
+    protected Female(string name): base (name)
+    {
+    }
+
+    public Female(string name, Female mother, Male father):base(name, mother, father)
     {
     }
 }
 
 public sealed class Adam : Male
 {
-    private static readonly Adam instance = new Adam();
+    private static Adam adam;
+    private static Eve eve;
 
-    private Adam() : base("Adam", null, null)
+    private Adam(): base ("Adam")
     {
     }
 
     public static Adam GetInstance()
     {
-        return instance;
-    }
-}
-
-public sealed class Eve : Female
-{
-    private static Eve instance;
-    private readonly Adam adam;
-
-    private Eve(Adam adam) : base("Eve", null, null)
-    {
-        this.adam = adam ?? throw new ArgumentNullException(nameof(adam));
-    }
-
-    public static Eve GetInstance(Adam adam)
-    {
-        if (adam == null)
-        {
-            throw new ArgumentNullException(nameof(adam), "Eve can only be created from Adam's rib");
-        }
-
-        if (instance == null)
-        {
-            instance = new Eve(adam);
-        }
-        return instance;
+        return adam??(adam=new Adam());
     }
 }
